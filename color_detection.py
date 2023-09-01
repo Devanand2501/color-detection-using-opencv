ba746b249c38527e891e49662432a071cc1d825c
Devanand <devanandkangane@gmail.com>
2023-09-01 21:43:32 +0530

    Updated with color name and draw function

--- color_detection.py
+++ color_detection.py
@@ -12,14 +12,11 @@ def rescaleFrame(frame, scale = 0.75):
     return cv2.resize(frame,dimenssions,interpolation=cv2.INTER_AREA)
 
 img = rescaleFrame(img,scale=1)
-clicked = False
-r = g = b = x_pos = y_pos = 0
 
 index = ["color", "color_name", "HEX code", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
 csv.drop("color",axis=1,inplace=True)
-
 def get_color_name(R, G, B):
     minimum = 10000
     for i in range(len(csv)):
@@ -39,20 +36,3 @@ def draw_function(event, x, y, flags, param):
         b = int(b)
         g = int(g)
         r = int(r)
-
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw_function)
-
-while True:
-
-    cv2.imshow("image", img)
-    if clicked:
-        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
-        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
-        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-        if r + g + b >= 600:
-            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
-        clicked = False
-    if cv2.waitKey(20) & 0xFF == 8:
-        break
-cv2.destroyAllWindows()
